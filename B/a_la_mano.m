db232c8134eef2a6547b5b3adf763a4176b40c1e
Marc-Andrieu <marc.andrieu@outlook.com>
2025-04-01 11:39:20 +0200

    J'arrête pour aujd

--- B/a_la_mano.m
+++ B/a_la_mano.m
@@ -12,11 +12,10 @@ topright = [230, 798];
 bottomright = [468, 808];
 bottomleft = [522, 597];
 
-sourcePoints = [0, 0; 1000, 0; 1000, 1500; 0, 1500]
+sourcePoints = [0, 0; 0, 1000; 1500, 1000; 1500, 0]
 destinationPoints = [0, 0; topright - topleft; bottomright  - topleft; bottomleft  - topleft]
 
-syms a b
-eqn1 = a * b == ;
-eqn2 =  == ;
-[A ,B] = equationsToMatrix([eqn1, eqn2], [a, b])
-X = linsolve(A,B)
+a = ( - ) / 1500;
+b = (230 - 210) / 1000;
+c = ( - ) / ; 
+d = ( - ) / ;
